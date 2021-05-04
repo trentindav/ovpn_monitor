e0f893cc657fcd03e86602d45382ca75ac6ed29b
Davide Trentin <trentindav@gmail.com>
2021-05-04 17:54:51 -0400

    example resources, prettier

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
+const yaml = require("js-yaml");
+const fs = require("fs");
 
-const args = require('minimist')(process.argv.slice(2), {
-  string: ['config'],
-  default: { config: './ovpn_monitor_config.yaml' },
+const args = require("minimist")(process.argv.slice(2), {
+  string: ["config"],
+  default: { config: "./ovpn_monitor_config.yaml" },
 });
 
-try {
-  const doc = yaml.load(fs.readFileSync(args.config, 'utf-8'));
-  console.log(doc);
-} catch (err) {
-  console.error(err);
+function main() {
+  try {
+    const doc = yaml.load(fs.readFileSync(args.config, "utf-8"));
+    console.log(doc)
+  } catch (err) {
+    console.error(err);
+  }
 }
+
+main();
+
